3c2f55bc46f205b36247799066f76da5e3be3956
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2025-12-30 00:02:37 +0530

    refactor: remove ClerkProvider from layout and dependencies in package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,9 +25,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
           {children}
-        </ClerkProvider>
       </body>
     </html>
   );
